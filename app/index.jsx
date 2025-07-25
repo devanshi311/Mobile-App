d1d73a153207c833c518446e03636a31fec5d348
devanshi311 <devanshigodhaniya311@gmail.com>
2025-07-25 23:34:43 +0530

    Initial commit: Added React Native code

---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}>
+      <Text>
+        Edit app/index.tsx to edit this screen.12nn33
+      </Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:"yellow",
+  },
+});
